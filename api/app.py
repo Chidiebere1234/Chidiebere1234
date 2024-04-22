69180189e2122a348094d2c1d70fb995ff3506e7
Sulaimon <abodunrinsulaimon1@gmail.com>
2024-04-22 11:28:06 +0100

    Swagger API UI Foundation Added

--- api/app.py
+++ api/app.py
@@ -54,8 +54,10 @@ def create_app() -> Flask:
         print("Database initialized.")
 
     SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
-    API_URL = 'http://petstore.swagger.io/v2/swagger.json'  # Our API url (can of course be a local resource)
+    # API_URL = 'http://petstore.swagger.io/v2/swagger.json'  # Our API url (can of course be a local resource)
+    API_URL = 'http://127.0.0.1:5005'
 
+    # Doc: https://pypi.org/project/flask-swagger-ui/
     # Call factory function to create our blueprint
     swaggerui_blueprint = get_swaggerui_blueprint(
         SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
